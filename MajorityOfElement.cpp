76d7e3b143aebcb3bf360cfaad620e1a3c9ef0b8
Pushpendra Dhakad <pushpendra.dhakad_cs25@gla.ac.in>
2026-01-21 16:34:51 +0530

    [Pushpendra] Commit

---
+++ MajorityOfElement.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>&nums) {
+        int count =0;
+        int candidate=0;
+       for(int num:nums){
+        if(count==0){
+        candidate =num;} 
+	   count+=(num==candidate)?1:-1;}
+     
+    return candidate;}
+};
+int main(){
+    Solution obj;
+    vector<int>nums={2,2,1,1,1,2,2};
+    cout<<"Majority Element:"<< obj.majorityElement(nums);
+    return 0;
+}
