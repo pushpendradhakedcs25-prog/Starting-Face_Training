79268c62a85338428f3b226ef277f769a0f6cc9a
Pushpendra Dhakad <pushpendra.dhakad_cs25@gla.ac.in>
2026-02-04 22:09:17 +0530

    add file

---
+++ array_odd_even.java
@@ -0,0 +1,16 @@
+class array_3{
+public static void main(String arg[]){
+int a[ ] = new int[5];
+    a[0]=99;
+	a[1]=67;
+	a[2]=108;
+	a[3]=45;
+	a[4]=70;
+	int sum=0;
+System.out.println("printing  linear search array:");
+for(int i =a.length -1 ; i>=0; i--){
+	if(a[i]%2==0){
+	System.out.prinitln("even"a[i]);}
+	else{
+	System.out.println("odd"a[i]);}
+}}}
